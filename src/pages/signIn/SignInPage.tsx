0bee242debf4246977cafc803aa08b5eb9a88a4b
sunShineLoveMe <juneandrao@outlook.com>
2023-07-24 21:15:06 +0800

    新增登陆表单数据

--- src/pages/signIn/SignInPage.tsx
+++ src/pages/signIn/SignInPage.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { UserLayout } from "../../layouts/userLayout";
+import { SignInForm } from "./SignInForm";
 
 export const SignInPage: React.FC = () => {
     return (
         <UserLayout>
-            <div>登陆页面</div>
+            <SignInForm />
         </UserLayout>
     )
 }
