18394c884250d70a3f9b3ce820402c44b75adf18
sunShineLoveMe <juneandrao@outlook.com>
2023-07-25 17:06:08 +0800

    增加注销功能

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,11 +6,14 @@ import { GlobalOutlined } from "@ant-design/icons"
 import { useParams, useLocation, useNavigate } from "react-router-dom";
 import { useSelector } from "../../redux/hooks";
 import { useDispatch } from "react-redux";
-import { addLanguageActionCreator, 
-        changeLanguageActionCreator 
-      } from "../../redux/language/languageActions";
-import { useTranslation } from "react-i18next";    
-import jwt_decode, { JwtPayload as DefaultJwtPayload } from 'jwt-decode'  
+import {
+  addLanguageActionCreator,
+  changeLanguageActionCreator
+} from "../../redux/language/languageActions";
+import { useTranslation } from "react-i18next";
+import jwt_decode, { JwtPayload as DefaultJwtPayload } from 'jwt-decode'
+import { use } from "i18next";
+import { userSlice } from "../../redux/user/slice";
 
 interface JwtPayload extends DefaultJwtPayload {
   username: string
@@ -29,18 +32,24 @@ export const Header: React.FC = () => {
   const [username, setUsername] = useState("")
 
   useEffect(() => {
-    if(jwt) {
+    if (jwt) {
       const token = jwt_decode<JwtPayload>(jwt)
       setUsername(token.username)
     }
   }, [jwt])
 
   const menuClickHandler = (e) => {
-    if(e.key === 'new') {
+    if (e.key === 'new') {
       dispatch(addLanguageActionCreator('新语言', 'new_lang'))
     } else {
       dispatch(changeLanguageActionCreator(e.key))
     }
+  }
+
+  const onLogout = () => {
+    dispatch(userSlice.actions.logOut())
+    navigate("/")
+    window.location.reload()
   }
 
   return (
@@ -64,12 +73,22 @@ export const Header: React.FC = () => {
           >
             {language === "zh" ? "中文" : "English"}
           </Dropdown.Button>
-          <Button.Group className={styles["button-group"]}>
-            <Button onClick={() => navigate("/register")}>{t("header.register")}</Button>
-            <Button onClick={() => navigate("/signin")}>{t("header.signin")}</Button>
-          </Button.Group>
+          {
+            jwt ?
+              <Button.Group className={styles["button-group"]}>
+                <span>{t("header.welcome")}
+                  <Typography.Text strong>{username}</Typography.Text>
+                </span>
+                <Button>{t("header.shoppingCart")}</Button>
+                <Button onClick={onLogout}>{t("header.signOut")}</Button>
+              </Button.Group>
+              :
+              <Button.Group className={styles["button-group"]}>
+                <Button onClick={() => navigate("/register")}>{t("header.register")}</Button>
+                <Button onClick={() => navigate("/signin")}>{t("header.signin")}</Button>
+              </Button.Group>
+          }
         </div>
-
       </div>
       <Layout.Header className={styles["main-header"]}>
         <span onClick={() => navigate("/")}>
@@ -82,7 +101,7 @@ export const Header: React.FC = () => {
           className={styles["search-input"]}
           placeholder={'请输入旅游目的地、主题、或关键字'}
           onSearch={(keyword) => navigate(`/search/${keyword}`)}
-          />
+        />
       </Layout.Header>
       <Menu className={styles['main-menu']}
         mode={"horizontal"}
--- src/redux/user/slice.ts
+++ src/redux/user/slice.ts
@@ -32,7 +32,11 @@ export const userSlice = createSlice({
     name: 'user',
     initialState,
     reducers: {
-
+        logOut: (state) => {
+            state.token = null;
+            state.error = null;
+            state.loading = false;
+        }
     },
     extraReducers: {
         [signIn.pending.type]: (state) => {
